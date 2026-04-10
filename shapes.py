0c81681d5689d55f64b30a17bbee98f2d9ab5336
thinkritesh <gholapritesh9761@gmail.com>
2026-04-10 12:39:19 +0530

    Create shapes.py

---
+++ shapes.py
@@ -0,0 +1,11 @@
+
+import math
+def circle_area(radius):
+area = math.pi * radius * radius
+return area
+def rectangle_area(length, width):
+area = length * width
+return area
+def triangle_area(base, height):
+area = 0.5 * base * height
+return area
